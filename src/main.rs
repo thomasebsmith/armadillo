10d5171ed1d6af9d5de3cd87903bde0e85a79699
Thomas Smith <thsm@umich.edu>
2019-04-24 19:16:39 -0400

    Fix indentation in main.rs and lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::process;
 
 fn main() {
-	if let Err(error) = armadillo::run() {
-		println!("{}", error);
-		process::exit(1);
-	}
+    if let Err(error) = armadillo::run() {
+        println!("{}", error);
+        process::exit(1);
+    }
 }
